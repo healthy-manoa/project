fac214b0c8055ec54603b5d7aabfd109bb0ceb62
Rexter Delos Santos <rexterds@hawaii.edu>
2019-11-19 13:26:43 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,34 +1,10 @@
 import React from 'react';
 import _ from 'lodash';
-import { Grid, Image, Search } from 'semantic-ui-react';
-
-const initialState = { isLoading: false, results: [], value: '' };
+import { Grid, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
-class Landing extends React.Component {
-  state = initialState;
-
-  handleResultSelect = (e, { result }) => this.setState({ value: result.name });
-
-  handleSearchChange = (e, { value }) => {
-    this.setState({ isLoading: true, value });
-
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.setState(initialState);
-
-      const re = new RegExp(_.escapeRegExp(this.state.value), 'i');
-      const isMatch = (result) => re.test(result.name);
-
-      this.setState({
-        isLoading: false,
-        results: _.filter(source, isMatch),
-      });
-    }, 300);
-  }
-
+class Landing extends React.Component { 
   render() {
-    const { isLoading, value, results } = this.state
-
     return (
         <div className='background'>
           <Grid verticalAlign='bottom' textAlign='center' container>
