55798fdaad502159d6ba1bfbd51924609769ed05
TimothyNicdao <tmnic80833@gmail.com>
2019-11-20 00:04:36 -1000

    vendor page initial

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Inventories } from '../../api/inventory/Inventories';
 
 /* eslint-disable no-console */
 
@@ -14,5 +15,18 @@ if (Stuffs.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+
+function addInventory(data) {
+  console.log(`  Adding: ${data.vendor} (${data.owner})`);
+  Inventories.insert(data);
+}
+
+if (Inventories.find().count() === 0) {
+  if (Meteor.settings.defaultInventories) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultInventories.map(data => addInventory(data));
   }
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { Inventories } from '../../api/inventory/Inventories';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Stuff', function publish() {
@@ -15,6 +16,22 @@ Meteor.publish('Stuff', function publish() {
 Meteor.publish('StuffAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.find();
+  }
+  return this.ready();
+});
+
+
+Meteor.publish('Inventories', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Inventories.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish('InventoriesAdmin', function publish() {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Inventories.find();
   }
   return this.ready();
 });
