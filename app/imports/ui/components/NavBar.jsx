be78382a98fb05e81b16a176f3e066305d1999b4
TimothyNicdao <tmnic80833@gmail.com>
2019-12-06 11:03:26 -1000

    Update NavBar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -37,6 +37,15 @@ class NavBar extends React.Component {
         <Menu.Item as={NavLink} activeClassName="" exact to="/vendor">
           <Header as='h4'><Icon name='shop'/>Vendors</Header>
         </Menu.Item>
+        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+            <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
+        ) :  {this.props.currentUser ? (
+              [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Recipes</Menu.Item>,
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Recipes</Menu.Item>,
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/profile" key='profile'>Profile</Menu.Item>,
+                <Menu.Item as={NavLink} activeClassName="active" exact to="/list-inventory" key= 'inventory' >Inventory</Menu.Item>,
+              ]
+          ) : ''}  }
         {this.props.currentUser ? (
             [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Recipes</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Recipes</Menu.Item>,
