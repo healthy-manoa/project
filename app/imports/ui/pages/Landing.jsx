bb6ef4a69291dfab562a73b78e30b63ab2752a7d
Rexter Delos Santos <rexterds@hawaii.edu>
2019-12-17 13:23:13 -1000

    Search function works for vendors

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,44 +1,27 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import _ from 'lodash';
-import { Grid, Image, Search, Label, Dropdown } from 'semantic-ui-react';
+import { Grid, Image, Search, Label } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
+import { withRouter, Redirect } from 'react-router-dom';
 import { Recipes } from '../../api/recipe/Recipes';
 import { Vendors } from '../../api/vendor/Vendors';
 
-const searchOptions = [
-  {
-    key: 'Vendors',
-    text: 'Vendors',
-    name: 'Vendors',
-    value: 'Vendors',
-  },
-  {
-    key: 'Recipes',
-    text: 'Recipes',
-    name: 'Recipes',
-    value: 'Recipes',
-  },
-];
+const initialState = { isLoading: false, results: [], value: '', redirect: false, id: '' };
 
 const resultRenderer = ({ name }) => <Label content={name}/>;
 
 resultRenderer.propTypes = {
   name: PropTypes.string,
-  description: PropTypes.string,
 };
-
-const initialState = { isLoading: false, results: [], value: '' };
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   state = initialState;
 
-  handleDropChange = (e, { value }) => this.setState({ dropValue: value });
-
   handleResultSelect = (e, { result }) => {
-    this.setState({ value: result.name });
+    this.setState({ redirect: true, id: result._id });
   }
 
   handleSearchChange = (e, { value }) => {
@@ -47,16 +30,12 @@ class Landing extends React.Component {
     setTimeout(() => {
       if (this.state.value.length < 1) return this.setState(initialState);
 
-      let source;
-      if (this.state.dropValue === 'Recipes') source = this.props.recipes;
-      else source = this.props.vendors;
-
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i');
       const isMatch = (result) => re.test(result.name);
 
       return this.setState({
         isLoading: false,
-        results: _.filter(source, isMatch),
+        results: _.filter(this.props.vendors, isMatch),
       });
     }, 300);
   }
@@ -70,29 +49,26 @@ class Landing extends React.Component {
             <Grid.Row>
               <Image size='medium' centered src='/images/logo.png'/>
             </Grid.Row>
-            <Grid.Row columns={5}>
-              <Grid.Column>
-                <Dropdown
-                    placeholder='Choose what to search'
-                    selection
-                    options={ searchOptions }
-                    onChange={this.handleDropChange}
-                />
-              </Grid.Column>
-              <Grid.Column>
-                <Search
-                    size='huge'
-                    onResultSelect={this.handleResultSelect}
-                    onSearchChange={_.debounce(this.handleSearchChange, 500, {
-                      leading: true,
-                    })}
-                    results={results}
-                    value={value}
-                    resultRenderer={resultRenderer}
-                />
-              </Grid.Column>
+            <Grid.Row>
+              <Search
+                  size='huge'
+                  onResultSelect={this.handleResultSelect}
+                  onSearchChange={_.debounce(this.handleSearchChange, 500, {
+                    leading: true,
+                  })}
+                  results={results}
+                  value={value}
+                  resultRenderer={resultRenderer}
+              />
             </Grid.Row>
           </Grid>
+          <div>
+            {this.state.redirect &&
+              <Redirect to={{
+                pathname: `/individual-vendor/${this.state.id}`,
+              }}/>
+            }
+          </div>
         </div>
     );
   }
@@ -101,10 +77,11 @@ class Landing extends React.Component {
 Landing.propTypes = {
   recipes: PropTypes.array.isRequired,
   vendors: PropTypes.array.isRequired,
+  location: PropTypes.object.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
+const LandingContainer = withTracker(() => {
   // Get access to Stuff documents.
   Meteor.subscribe('PublicRecipes');
   Meteor.subscribe('Vendors');
@@ -113,3 +90,5 @@ export default withTracker(() => {
     vendors: Vendors.find({}).fetch(),
   };
 })(Landing);
+
+export default withRouter(LandingContainer);
