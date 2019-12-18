ee29c4a044e7ab03d0dd912ac23f10c20f69fa94
weixiao hu <weixiaoh@hawaii.edu>
2019-12-17 19:31:59 -1000

    ingredients array

--- app/imports/ui/components/PublicRecipe.jsx
+++ app/imports/ui/components/PublicRecipe.jsx
@@ -13,9 +13,9 @@ class PublicRecipe extends React.Component {
           <Card.Content>
             <Card.Header>{this.props.recipe.name}</Card.Header>
             <Card.Description>Description: {this.props.recipe.description}</Card.Description>
-            <Card.Description>Ingredients:{this.props.recipe.ingredients}</Card.Description>
+            <Card.Description>Ingredients:{this.props.recipe.ingredients.join(', ')}</Card.Description>
             <Card.Description>Steps: {this.props.recipe.steps}</Card.Description>
-            <Card.Description>Tags: {this.props.recipe.tags}</Card.Description>
+            <Card.Description>Tags: {this.props.recipe.tags.join(' || ')}</Card.Description>
             <Card.Meta>{this.props.recipe.owner}</Card.Meta>
           </Card.Content>
         </Card>
