d6d756a35294126f3580328cdd6a488988a6fd5d
haochen-wang <54867326+haochen-wang@users.noreply.github.com>
2019-12-17 19:15:26 -1000

    submit function

--- app/imports/ui/pages/Review.jsx
+++ app/imports/ui/pages/Review.jsx
@@ -9,15 +9,18 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { Reviews, ReviewSchema } from '/imports/api/review/Reviews';
+import swal from 'sweetalert';
 
 /** A simple static component to render some text for the landing page. */
 class Review extends React.Component {
+
   render() {
     return (
+        <div className='vendor-background'>
         <div className='content-wrap'>
           <Grid container centered>
             <Grid.Column>
-              <Header as="h2" textAlign="center">Feedback</Header>
+              <Header as="h2" textAlign="center" inverted>Feedback</Header>
               <AutoForm schema={ReviewSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
                 <Segment>
                   <TextField name='name'/>
@@ -28,6 +31,7 @@ class Review extends React.Component {
               </AutoForm>
             </Grid.Column>
           </Grid>
+        </div>
         </div>
     );
   }
