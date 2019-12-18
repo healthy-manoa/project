25f3dc5c4129a3030f7f4e877d2e0649c01b0eb5
TimothyNicdao <tmnic80833@gmail.com>
2019-12-17 17:21:14 -1000

    commit

--- app/imports/ui/pages/ShowVendorsAdmin.jsx
+++ app/imports/ui/pages/ShowVendorsAdmin.jsx
@@ -27,9 +27,6 @@ class ShowVendors extends React.Component {
                 vendor={vendors}
                 />)}
           </Card.Group>
-          <Card>
-            <Image src={'https://imageog.flaticon.com/icons/png/512/61/61112.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'} />
-          </Card>
         </Container>
   </div>
         </div>
